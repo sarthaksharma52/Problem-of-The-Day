bbe5520e8f6500e5c28a7cfaf3213573e2db4361
riya-rjha <riyaranjanjha2006@gmail.com>
2023-11-28 21:04:49 +0530

    Day 1 Problem Solved

---
+++ Week 1/Week1.md
@@ -0,0 +1,14 @@
+### Week 1 Problem Statements
+
+### Day 1
+### Sum of dependencies in a graph
+Given a directed graph with V nodes and E edges, if there is an edge from u to v, then we will say that u depends on v. Number of Dependencies (NoD) for a node x is the total count of nodes that x depends upon. Find out the sum of number of dependencies of every node.
+
+Input:
+V = 4
+E = 4
+Edges = { {0,2}, {0,3}, {1,3}, {2,3} }
+
+Output : 4
+
+### Day 2
--- Week 1/sumGraph.java
+++ Week 1/sumGraph.java
@@ -1,5 +1,34 @@
-public class sumGraph{
-    public static void main(String[]args){
-        
+import java.util.ArrayList;
+import java.util.List;
+
+public class sumGraph {
+
+    public static int sumOfDependencies(List<List<Integer>> adj, int V) {
+        // code here
+        int[] dependencies = new int[V];
+
+        // Iterate through the adjacency list and update the dependencies array
+        for (int i = 0; i < V; i++) {
+            for (int j : adj.get(i)) {
+                dependencies[j]++;
+            }
+        }
+
+        // Calculate the sum of dependencies
+        int sum = 0;
+        for (int dependency : dependencies) {
+            sum += dependency;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> lst = new ArrayList<>();
+        int V = 4;
+        lst.add(List.of(0, 2));
+        lst.add(List.of(0, 3));
+        lst.add(List.of(1, 3));
+        lst.add(List.of(2, 3));
+        System.out.println(sumOfDependencies(lst, V));
     }
 }
